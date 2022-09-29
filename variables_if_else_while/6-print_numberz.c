f2bc13b3fabdd727eab781697f696b2ee7f7aacc
IVanEDL <ivanaq3d@gmail.com>
2022-09-29 06:55:08 -0700

    P6 T6 Te1

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this code will dispay all numbers from 0 to 9
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		putchar(c);
+	}
+	putchar(10);
+	return (0);
+}
