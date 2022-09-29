9ba18c058bc096456d12ee1f3e8d78a5e014f258
IVanEDL <ivanaq3d@gmail.com>
2022-09-29 06:24:51 -0700

    P6 T2 Te3

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this code will display all letters of the alphabet, lowercase
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
