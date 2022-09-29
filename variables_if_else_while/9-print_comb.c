4ee449a33ace6391440eac84d308cdfadd2e1841
IVanEDL <ivanaq3d@gmail.com>
2022-09-29 07:20:37 -0700

    P6 T9 Te1

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - this code will dispay all numbers from 0 to 9
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		putchar(c);
+		if(c != '9')
+		{
+			putchar(',');
+			putchar(32);
+		}
+	}
+	putchar(10);
+	return (0);
+}
