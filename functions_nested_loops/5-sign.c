99330073afa21a3ccb5784e15c3ad2354f055e14
IVanEDL <ivanaq3d@gmail.com>
2022-09-30 11:29:30 -0700

    P11 T5 Te2

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,13 +11,13 @@ int print_sign(int n)
 	if (n > '0')
 	{
 		putchar('+');
-		return (1);
+			return (1);
 	}
 	if (n < '0')
 	{
 		putchar('-');
-		return (-1);
+			return (-1);
 	}
 	putchar('0');
-	return (0);
+		return (0);
 }
