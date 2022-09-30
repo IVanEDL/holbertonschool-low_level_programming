39532063d354e15864ea8a71d50f4949e6c355e9
IVanEDL <ivanaq3d@gmail.com>
2022-09-30 11:26:31 -0700

    P11 T5 Te1

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: entry number
+ * Return: depends
+ */
+
+int print_sign(int n)
+{
+	if (n > '0')
+	{
+		putchar('+');
+		return (1);
+	}
+	if (n < '0')
+	{
+		putchar('-');
+		return (-1);
+	}
+	putchar('0');
+	return (0);
+}
