03602252ae61a5204d64a07e7266aad19d64e294
IVanEDL <ivanaq3d@gmail.com>
2022-10-03 06:54:40 -0700

    P11 T9 Te1

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int f = 0;
+	int c = 0;
+
+	for (f = 0; c < 9; c++)
+	{
+		for (c = 0; c < 9; c++)
+		{
+			if ((f * c < 10) && (f * (c +  1) >= 10))
+			{
+				_putchar(f * c + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (f * c >= 10)
+			{
+				_putchar((f * c) / 10 + '0');
+				_putchar((f * c) % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(f * c + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			if (f * c < 10)
+				_putchar(f * c + '0');
+			else
+			{
+				_putchar((f * c) / 10 + '0');
+				_putchar((f * c) % 10 + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
