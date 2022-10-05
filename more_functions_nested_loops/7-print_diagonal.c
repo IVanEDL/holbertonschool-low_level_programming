e034e0e6cbe7f0074b4f8e6780097551ec78ad37
IVanEDL <ivanaq3d@gmail.com>
2022-10-05 12:47:01 -0700

    P13 T7 Te1

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line of variable lenght
+ * @n: line lenght
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n > 0)
+	{
+		for (a = 0 ; a < n; a++)
+		{
+			for (b = 0 ; b < a; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
