4e05b9a01408c37726f85cf38344fd53f0d54f93
IVanEDL <ivanaq3d@gmail.com>
2022-10-11 09:46:35 -0700

    P15 T1 Te2

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_strncat - concatenates the value of a string to another string with a limit
+ * *_strncat - concatenates the value of a string to another string with limit
  * @src: copied string
  * @dest: destination string
  * @n: limit of bytes
@@ -16,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		i++;
 	}
-	for (count = 0; count < n && src [count] != '\0'; count++)
+	for (count = 0; count < n && src[count] != '\0'; count++)
 		dest[i + count] = src[count];
 	dest[i + count] = '\0';
 	return (dest);
