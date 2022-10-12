970404d290a68ed0f4d9925b74166e0211837b56
IVanEDL <ivanaq3d@gmail.com>
2022-10-12 05:54:11 -0700

    P14 T5 Te2

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,13 +1,13 @@
 /**
  * *string_toupper - turns all lowercase to uppercase
- * @*: entry string
+ * @a: entry string
  * Return: said char
  */
 
 char *string_toupper(char *a)
 {
 	int i;
-	
+
 	for (i = 0; a[i] != 0; i++)
 	{
 		if (a[i] >= 97 && a[i] <= 122)
