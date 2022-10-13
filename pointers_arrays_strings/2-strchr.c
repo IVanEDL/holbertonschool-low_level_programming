5df6f098fb11fad0efda61ed6e8f377eefad9b86
IVanEDL <ivanaq3d@gmail.com>
2022-10-13 09:13:30 -0700

    P15 T2 Te1

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+/**
+ * _strchr - locates a character in a string
+ * @s: string to be checked
+ * @c: character to find
+ * Return: pointer to the first ocurrence of c in the string s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			for (; s[i] != '\0'; i++)
+				return (&s[i]);
+			break;
+		}
+	}
+	return ('\0');
+}
