b461d63f1748eb1eb1eedc2b9a9f911109789da1
IVanEDL <ivanaq3d@gmail.com>
2022-10-13 10:01:39 -0700

    P15 T3 Te1

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string
+ * @accept: bytes to check about
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	int e, f, bol;
+
+	for (e = 0; s[e] != '\0'; e++)
+	{
+		bol = 1;
+
+		for (f = 0; accept[f] != '\0'; f++)
+		{
+			if (accept[f] == s[e])
+			{
+				i++;
+				bol = 0;
+			}
+		}
+		if (bol == 1)
+		{
+			break;
+		}
+	}
+	return (i);
+}
