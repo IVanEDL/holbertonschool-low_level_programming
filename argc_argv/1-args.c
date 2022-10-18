16ab95d3fae04f05651810a0511cd789da671db5
IVanEDL <ivanaq3d@gmail.com>
2022-10-18 09:18:07 -0700

    P17 T1 Te1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -2,14 +2,14 @@
 
 /**
  * main - prints the number of arguments passed into it
- * @args - number of arguments in input
- * @argv - array of pointers to the arguments
+ * @argc: number of arguments in input
+ * @argv: array of pointers to the arguments
  * Return: 0
  */
 
 int main(int argc, char **argv __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
-	
+
 	return (0);
 }
