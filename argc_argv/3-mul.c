d7672c0def31d2cfd25f9c7277324d9ffa992a9a
IVanEDL <ivanaq3d@gmail.com>
2022-10-18 09:37:36 -0700

    P17 T3 Te1

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of arguments in input
+ * @argv: array of pointers to the arguments
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int i;
+		i = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
