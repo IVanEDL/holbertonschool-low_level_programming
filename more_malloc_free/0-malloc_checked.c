9f35c8b27ed4e55a14a552aced705b3b40fe8ef5
IVanEDL <ivanaq3d@gmail.com>
2022-10-21 09:32:56 -0700

    P19 T0 Te1

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: ammount of bytes to be allocated
+ * Return: Pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *mem = 00;
+	char i = 98;
+
+	mem = malloc(sizeof(b));
+	if (mem == 00)
+		exit(i);
+	return (mem);
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+In this projects, we do the tasks corresponding to the project 19: more malloc free. 
