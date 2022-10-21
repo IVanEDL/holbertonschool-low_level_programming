a47fabd0b8968c87a1432cb0f85538eddb1e0e2a
IVanEDL <ivanaq3d@gmail.com>
2022-10-21 11:13:18 -0700

    P19 T1 Te5

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,106 +12,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *strdes = 00;
 	int i = 0;
 	unsigned int a;
+	int p;
+	unsigned int r;
 
 	if (s1 == 00)
 		s1 = "";
 	if (s2 == 00)
 		s2 = "";
-	if (_strlenprime(s2) >= n)
+	for (p = 0; s1[p] != '\0'; p++)
+		;
+	for (r = 0; s2[r] != '\0'; r++)
+		;
+	if (r >= n)
 		i = n;
-	else if (_strlenprime(s2) < n)
-		i = _strlen(s2);
-	strdes = malloc((_strlen(s1) + i + 1) * sizeof(char));
+	strdes = malloc((i + p + 1) * sizeof(char));
 	if (strdes == 00)
 		return (strdes);
 	for (a = 0; s1[a] != '\0'; a++)
 		strdes[a] = s1[a];
 	for (a = 0; a < n && s2[a] != '\0'; a++)
-		strdes[_strlen(s1) + a] = s2[a];
+		strdes[a + p] = s2[a];
 	return (strdes);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: entry value
- * Return: said length
- */
-
-int _strlen(char *s)
-{
-	int i;
-	int count = 0;
-
-	for (i = 0; s[i]; i++)
-	{
-		count++;
-	}
-	return (count);
-}
-
-/**
- * *_strcat - concatenates the value of a string to another string
- * @src: copied string
- * @dest: destination string
- * Return: dest
- */
-
-/**char *_strcat(char *dest, char *src)
-{
-	int i = 0, count = 0;
-
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	for (; src[count] != '\0';)
-	{
-		dest[i + count] = src[count];
-		count++;
-	}
-	dest[i + count] = '\0';
-	return (dest);
-}
-*/
-
-/**
- * *_strncat - concatenates the value of a string to another string with limit
- * @src: copied string
- * @dest: destination string
- * @n: limit of bytes
- * Return: dest
- */
-
-/**
- * char *_strncat(char *dest, char *src, int n)
-{
-	int i = 0, count;
-
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	for (count = 0; count < n && src[count] != '\0'; count++)
-		dest[i + count] = src[count];
-	dest[i + count] = '\0';
-	return (dest);
-}
-*/
-
-/**
- * _strlenprime - returns the length of a string
- * @s: entry value
- * Return: said length
- */
-
-unsigned int _strlenprime(char *s)
-{
-	int i;
-       	unsigned int count = 0;
-
-	for (i = 0; s[i]; i++)
-	{
-		count++;
-	}
-	return (count);
 }
