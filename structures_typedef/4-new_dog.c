45c0c40b4c1de8357ae308511fbe0cc86a512bd3
IVanEDL <ivanaq3d@gmail.com>
2022-10-27 13:44:08 -0700

    P22 T4 Te1

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,38 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include "_strdup.c"
+/**
+ * new_dog - creates a new dog
+ * @name: dog name 
+ * @age: dog age
+ * @owner: dog owner
+ * Return: pointer to the new dog, 00 if it fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dogcpy;
+
+	if (name != 00 && owner != 00)
+	{
+		dogcpy = malloc(sizeof(dog_t));
+		if (dogcpy == 00)
+			return (00);
+		dogcpy->name = _strdup(name);
+		if (dogcpy->name == 00)
+		{
+			free(dogcpy);
+			return (00);
+		}
+		dogcpy->age = age;
+		dogcpy->owner = _strdup(owner);
+		if (dogcpy->owner == 00)
+		{
+			free(dogcpy->name);
+			free(dogcpy);
+			return (00);
+		}
+	}
+	return (dogcpy);
+}
---
+++ structures_typedef/_strdup.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup - returns a pointer to a string which is a dupe of string str
+ * @str: string to be duplicated
+ * Return: pointer to the duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	char *dupe = 00;
+	int i = 0, count = 0, a = 0;
+
+	if (str == 00)
+		return (dupe);
+	for (i = 0; str[i]; i++)
+		count++;
+	dupe = malloc(sizeof(char) * count + 1);
+	if (dupe == 00)
+		return (dupe);
+	for (a = 0; a <= count; a++)
+		dupe[a] = str[a];
+	return (dupe);
+}
