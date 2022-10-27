ff039e9641ec366810da03a47538350cb74ae4fb
IVanEDL <ivanaq3d@gmail.com>
2022-10-27 12:19:51 -0700

    P22 T0 Te2

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef dog_h
 #define dog_h
 
+/**
+ * struct dog - structure of a dog
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ */
+
 struct dog
 {
 	char *name, *owner;
