2712885cd8308c65990f7c5f6b9ac5e0ba752fd0
IVanEDL <ivanaq3d@gmail.com>
2022-11-17 09:00:03 -0800

    P28 T5 Te1

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list_t list
+ * @head: pointer to the head of the list
+ * Return: negativo master
+ */
+
+void free_listint2(listint_t *head)
+{
+	if (head != NULL)
+	{
+		free_listint(*head);
+		*head = NULL;
+	}
+}
