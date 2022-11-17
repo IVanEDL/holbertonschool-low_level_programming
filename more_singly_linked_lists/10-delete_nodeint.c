5a528ebaf269b4104b2ba612c6333cc5525b061b
IVanEDL <ivanaq3d@gmail.com>
2022-11-17 10:32:07 -0800

    P28 T10 Te2

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -1,50 +1,41 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node index in a linked list.
- * @head: the linked list.
- * @index: the position of the node to be deleted.
+ * delete_nodeint_at_index - deletes a certain node of a listint_t list.
+ * @head: head of the list.
+ * @index: position of the deleted node
  * Return: 1 if succesful, -1 if not.
  */
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i;
-	listint_t *p = *head, *aux;
+	unsigned int i = 0;
+	listint_t *tmp = *head, *rm;
 
-	if (*head == 00)
+	if (!*head)
 		return (-1);
-	if (index == 0) /*If is to be erased the head node*/
+	if (index == 0)
 	{
-		aux = *head;
+		rm = *head;
 		*head = (*head)->next;
-		free(aux);
+		free(rm);
 		return (1);
 	}
-	for (i = 0; i < index; i++)
+	for (; tmp; tmp = tmp->next)
 	{
-		if (i == 0 && index == 1)
-		{
-			*head = (*head)->next;
-			free(p);
-		}
-		else
-		{
-			if (i == index - 1 && p != 00)
-			{
-				aux->next = p->next;
-				free(p);
-			}
-			else
-			{
-				aux = p;
-				if (aux == 00)
-					break;
-				p = p->next;
-			}
-		}
+		i++;
 	}
-	if (i < index)
+	if (index > i)
 		return (-1);
+	tmp = *head;
+	for (i = 0; i < index - 1; i++)
+	{
+		tmp = tmp->next;
+	}
+	rm = tmp->next;
+	tmp->next = tmp->next->next;
+	rm->next = NULL;
+	free(rm);
 	return (1);
+
 }
