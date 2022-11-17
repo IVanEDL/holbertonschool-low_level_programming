8a9ac72f37c54fc00a7453f0a162b4c70b6de17a
IVanEDL <ivanaq3d@gmail.com>
2022-11-17 09:14:32 -0800

    P28 T6 Te1

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t list
+ * @head: node to be deleted (f :c)
+ * Return: heads node data
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *aux = *head;
+	int i;
+
+	if (*head != 00)
+	{
+		i = (*head)->n;
+		*head = (*head)->next;
+		free(aux);
+	}
+	return (i);
+}
