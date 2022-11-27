c58c52db2762da1fac9bf199222ed2fddb47c322
IVanEDL <ivanaq3d@gmail.com>
2022-11-27 10:25:29 -0800

    P31 T5 Te1

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table to print
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	int i = 0;
+
+	if (ht != 00)
+	{
+		printf("{");
+		while (index < ht->size)
+		{
+			while (ht->array[index])
+			{	
+				if (i == 0)
+				{
+					printf("'%s': '%s'", ht->array[index]->key, ht->array[index]->value);
+					i++;
+				}
+				else
+					printf(", '%s': '%s'", ht->array[index]->key, ht->array[index]->value);
+				ht->array[index] = ht->array[index]->next;
+			}
+			index++;
+		}
+		printf("}\n");
+	}
+}
