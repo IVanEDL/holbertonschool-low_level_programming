ea843ab62bb8ed45cda12be98f3466fdd4c98771
IVanEDL <ivanaq3d@gmail.com>
2023-01-10 17:14:33 -0800

    Hotfix betty

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -18,7 +18,7 @@ int binary_search(int *array, size_t size, int value)
 		print_array(array, left, right);
 		while (left <= right)
 		{
-			middle = (left + right)/2;
+			middle = (left + right) / 2;
 			if (array[middle] < value)
 				left = middle + 1;
 			else if (array[middle] > value)
